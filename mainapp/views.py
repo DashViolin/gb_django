bbf03b21650e49106c1a953b34ffa33a9c765bdd
DashViolin <mymail@srv.org>
2022-05-23 00:11:28 +0300

    lesson_2

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+# from django.shortcuts import render
+# from django.http import HttpRequest, HttpResponse
+# from django.views.generic import View
+
+
+# class HelloWorldVIew(View):
+#     def get(self, *args, **kwargs):
+#         return HttpResponse("HW!")
+
+
+# def kwargs_check(request: HttpRequest, **kwargs):
+#     return HttpResponse(f"kwargs:<br>{kwargs}")
+
+
+class MainPageView(TemplateView):
+    template_name: str = "mainapp/index.html"
+
+
+class LoginPageView(TemplateView):
+    template_name: str = "mainapp/login.html"
+
+
+class NewsPageView(TemplateView):
+    template_name: str = "mainapp/news.html"
+
+
+class CoursesPageView(TemplateView):
+    template_name: str = "mainapp/courses_list.html"
+
+
+class DocSitePageView(TemplateView):
+    template_name: str = "mainapp/doc_site.html"
+
+
+class ContactsPageView(TemplateView):
+    template_name: str = "mainapp/contacts.html"
