3fa3bce500072ddf173f967b78bdd11bda4be3dc
DashViolin <mymail@srv.org>
2022-05-23 09:32:14 +0300

    lesson_2: исправлены пути в шаблонах и роутере

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -9,11 +9,11 @@ app_name = MainappConfig.name
 urlpatterns = [
     # path("", views.HelloWorldVIew.as_view()),
     # path("<str:word>/", views.kwargs_check),
-    path("", RedirectView.as_view(url="index.html")),
-    path("index.html", views.MainPageView.as_view()),
-    path("login.html", views.LoginPageView.as_view()),
-    path("news.html", views.NewsPageView.as_view()),
-    path("courses_list.html", views.CoursesPageView.as_view()),
-    path("doc_site.html", views.DocSitePageView.as_view()),
-    path("contacts.html", views.ContactsPageView.as_view()),
+    path("", RedirectView.as_view(url="index")),
+    path("index", views.MainPageView.as_view()),
+    path("login", views.LoginPageView.as_view()),
+    path("news", views.NewsPageView.as_view()),
+    path("courses-list", views.CoursesPageView.as_view()),
+    path("doc-site", views.DocSitePageView.as_view()),
+    path("contacts", views.ContactsPageView.as_view()),
 ]
