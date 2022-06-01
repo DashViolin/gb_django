624c6986b0d90fc9249a989c7002c5357aaf2059
DashViolin <mymail@srv.org>
2022-06-01 08:13:41 +0300

    some little fixes

--- config/settings.py
+++ config/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = os.environ.get("DJANGO_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split()
 
 
 # Application definition
